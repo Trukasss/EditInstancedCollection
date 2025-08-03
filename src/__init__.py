52e4984a12bbe67d0d9ce3c5564f3ea83026a5f7
Lukas Sabaliauskas <lukas_sabaliauskas@hotmail.com>
2025-08-03 17:14:00 +0200

    ft: switch to scene/layer rather than link

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Edit Instanced Collection",
     "description": "Quickly find the instanced collection source",
     "author": "Lukas Sabaliauskas <lukas_sabaliauskas@hotmail.com>",
-    "version": (0, 0, 1),
+    "version": (0, 0, 2),
     "blender": (4, 0, 0),
     "location": "Object Properties > Instancing",
     # "doc_url": "https://extensions.blender.org/add-ons/",
@@ -10,7 +10,7 @@ bl_info = {
 }
 
 import bpy
-from bpy.types import Operator, Context, Object, Collection, Panel
+from bpy.types import Operator, Context, Object, Collection, LayerCollection, Scene, ViewLayer
 
 
 def is_collection_instance(object: Object):
@@ -33,6 +33,19 @@ def frame_selected():
     with bpy.context.temp_override(window=window, area=area_3d, region=region):
         bpy.ops.view3d.view_selected(use_all_regions=True)
 
+
+def recursive_layer_collection_search(collection: Collection, layer_collections: bpy.types.CollectionProperty):
+    for lc in layer_collections:
+        lc: LayerCollection
+        if lc.collection == collection:
+            return lc
+    for lc in layer_collections:
+        found = recursive_layer_collection_search(collection, lc.children)
+        if found:
+            return lc
+    return None
+
+
 class EIC_OT_ShowSource(Operator):
     """Show collection instance's source"""
     bl_idname = "eic.show_source"
@@ -54,20 +67,59 @@ class EIC_OT_ShowSource(Operator):
         if not coll:
             self.report({"WARNING"}, f"Missing collection instance source")
             return {"CANCELLED"}
-        # Link to scene if necessary
-        is_linked = False
-        if coll not in context.scene.collection.children_recursive:
-            context.scene.collection.children.link(coll)
-            is_linked = True
-        # Select collection's objects
-        for obj in context.selected_objects:
+        msg = f"Found source '{coll.name}'"
+
+        # Find scene
+        scene = next((s for s in bpy.data.scenes if coll in s.collection.children_recursive), None)
+        is_new_scene = False
+        if not scene:
+            scene = bpy.data.scenes.new(f"({coll.name})")
+            scene.collection.children.link(coll)
+            is_new_scene = True
+        
+        # Search view layers for already "checked" layer collection
+        view_layer = None
+        layer_coll = None
+        for vl in scene.view_layers:
+            vl: ViewLayer
+            layer_coll = recursive_layer_collection_search(coll, vl.layer_collection.children)
+            if layer_coll and layer_coll.exclude == False: # "checked"
+                view_layer = vl
+                break
+        if layer_coll is None:
+            raise ValueError(f"Missing layer collection in '{scene.name}'")
+        if view_layer is None:
+            if context.scene == scene:
+                view_layer = context.view_layer
+            else:
+                view_layer = scene.view_layers[0]
+        
+        # Switch to scene & layer
+        if context.window.scene == scene:
+            msg += " > current scene"
+        else:
+            msg += f" > {'new' if is_new_scene else 'existing'} scene '{scene.name}'"
+            context.window.scene = scene
+        if context.window.view_layer == view_layer:
+            msg += " > current view layer"
+        else:
+            msg += f" > view layer '{view_layer.name}'"
+            context.window.view_layer = view_layer
+        
+        # Show and select source
+        coll.hide_select = False
+        coll.hide_viewport = False
+        layer_coll.exclude = False
+        view_layer.active_layer_collection = layer_coll
+        for obj in scene.objects:
             obj: Object
-            obj.select_set(False)
-        for obj in coll.all_objects:
-            obj.select_set(True)
-        # Try frame selection
+            if obj.name in coll.all_objects:
+                obj.select_set(True, view_layer=view_layer)
+            else:
+                obj.select_set(False, view_layer=view_layer)
         frame_selected()
-        self.report({"INFO"}, f"Found source collection '{coll.name}'" + (" and linked it to scene" if is_linked else ""))
+
+        self.report({"INFO"}, msg)
         return {"FINISHED"}
 
 
